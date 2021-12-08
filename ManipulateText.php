97e41f008203753f105bc9c04d4d60c8e7c29fbe
safwen-code <safwendjebbi1234@gmail.com>
2021-12-08 12:57:34 +0100

    play with text and get dates & hours

--- ManipulateText.php
+++ ManipulateText.php
@@ -7,12 +7,14 @@
     <title>Document</title>
 </head>
 <body>
+
     <!-- strlen => give the length of string -->
     <?php
     $recipe = 'give me the lengthb of this text';
     $length = strlen($recipe);
     echo $length . '</br>';
     ?>
+
      <!-- count => give me the nbr ele in tab -->
      <?php
      $tab = [
@@ -22,7 +24,36 @@
      $test = count($tab);
      echo $test . '</br>';
      ?>
+
      <!-- replace a string with another string -->
       <?php echo str_replace('c', 'C', 'le cass c est très bon'); ?>
+      
+      <!-- formate text =>sprintf -->
+        <?php
+        $recipe = [
+            'titel' => 'salade',
+            'author' => 'balkis',
+            'descr' => ' salde tunisienne',
+        ];
+        echo sprintf(
+            '%s par %s :%s',
+            $recipe['titel'],
+            $recipe['author'],
+            $recipe['descr']
+        ) . '</br>';
+        ?>
+        <!-- play with date -->
+        <?php
+        $year = date('Y');
+        $month = date('m');
+        $date = date('d');
+        echo sprintf('%s / %s / %s', $year, $month, $date);
+        ?>
+        <!-- play with hours -->
+        <?php
+        $hour = date('H');
+        $minut = date('i');
+        echo sprintf('%s : %s', $hour, $minut);
+        ?>
 </body>
 </html>
