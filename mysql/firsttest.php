fa087458eda7d381cef41287ed99e529d21c645b
safwen-code <safwendjebbi1234@gmail.com>
2021-12-09 14:34:34 +0100

    test val anonyme ? && val identifiant =:author in sqlQuery

--- mysql/firsttest.php
+++ mysql/firsttest.php
@@ -1,27 +1,30 @@
 <?php
-try
-{
-	// On se connecte à MySQL
-	$mysqlClient = new PDO('mysql:host=localhost;dbname=we_love_food;charset=utf8', 'root', 'root');
-}
-catch(Exception $e)
-{
-	// En cas d'erreur, on affiche un message et on arrête tout
-        die('Erreur : '.$e->getMessage());
+try {
+    // On se connecte à MySQL
+    $mysqlClient = new PDO(
+        'mysql:host=localhost;dbname=we_love_food;charset=utf8',
+        'root',
+        'root'
+    );
+} catch (Exception $e) {
+    // En cas d'erreur, on affiche un message et on arrête tout
+    die('Erreur : ' . $e->getMessage());
 }
 
 // Si tout va bien, on peut continuer
-
+// val anonym ==> ?
+//val identifier ==> =:author
 // On récupère tout le contenu de la table recipes
-$sqlQuery = 'SELECT * FROM recipes WHERE is_enabled =1 ';
+$sqlQuery = 'SELECT * FROM recipes WHERE author =:author ';
 $recipesStatement = $mysqlClient->prepare($sqlQuery);
-$recipesStatement->execute();
+$recipesStatement->execute([
+    'author' => $_GET['author'],
+    
+]);
 $recipes = $recipesStatement->fetchAll();
 
 // On affiche chaque recette une à une
-foreach ($recipes as $recipe) {
-?>
+foreach ($recipes as $recipe) { ?>
     <p><?php echo $recipe['author']; ?></p>
-<?php
-}
+<?php }
 ?>
